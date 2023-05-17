2643f127cf01c09e6c4b72e5a2b3302a81e8ae10
mlgmontage <kettleofrassel1@gmail.com>
2023-05-17 17:54:53 +0600

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Hello from "./lib/Hello.svelte";
+</script>
+
+<main>
+  <h1>Svelte todo app</h1>
+  <Hello />
+</main>
+
+<style>
+</style>
---
+++ src/lib/Hello.svelte
@@ -0,0 +1,9 @@
+<main>
+  <h1>Hello world</h1>
+</main>
+
+<style>
+  h1 {
+    color: red;
+  }
+</style>
