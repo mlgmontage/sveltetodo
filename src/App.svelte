204ba8c97bb845b5cab8699aff93e18db720efa3
mlgmontage <kettleofrassel1@gmail.com>
2023-05-17 18:18:45 +0600

    feat: counter app

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+  import Counter from "./lib/Counter.svelte";
   import Hello from "./lib/Hello.svelte";
 </script>
 
 <main>
   <h1>Svelte todo app</h1>
   <Hello />
+  <Counter />
 </main>
 
 <style>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  let count: number = 1;
+
+  const inc = () => {
+    count += 1;
+  };
+
+  const dec = () => {
+    count -= 1;
+  };
+</script>
+
+<main>
+  <div class="counter">
+    <button on:click={inc} class="counter_button">+1</button>
+    <h1 class={`counter_title ${count > 0 ? "hot" : "cold"}`}>{count}</h1>
+    <button on:click={dec} class="counter_button">-1</button>
+  </div>
+</main>
+
+<style>
+  .counter {
+    display: flex;
+    gap: 1rem;
+    align-items: center;
+  }
+
+  .counter_button {
+    background-color: transparent;
+    border: 2px solid black;
+    border-radius: 3px;
+    padding: 5px;
+  }
+
+  .counter_title {
+    font-size: 4rem;
+  }
+
+  .hot {
+    color: red;
+  }
+
+  .cold {
+    color: blue;
+  }
+</style>
