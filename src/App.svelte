f25dd1e30151445995a2737bff6064715eb31e7b
mlgmontage <kettleofrassel1@gmail.com>
2023-05-19 14:02:24 +0600

    feat: reverse interger program

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import Hello from "./lib/Hello.svelte";
   import Nav from "./lib/Nav.svelte";
   import Todo from "./lib/Todo.svelte";
 </script>
---
+++ src/lib/Reverse.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  let input = "";
+  let output = "";
+
+  $: {
+    if (isNaN(Number(input))) {
+      output = "Not a number";
+    }
+
+    // with using string
+    output = input.split("").reverse().join("");
+  }
+</script>
+
+<main class="container">
+  <textarea bind:value={input} class="input" />
+  <div class="output">{output}</div>
+</main>
+
+<style>
+  .container {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 1rem;
+    margin-inline: 1rem;
+    height: 300px;
+  }
+
+  .output {
+    border: 1px solid black;
+  }
+</style>
